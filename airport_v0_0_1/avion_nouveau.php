39cc6b05538910c2a94cf34e6b217cf77ec0d632
WillStanks <william4800@hotmail.com>
2022-02-17 13:50:14 -0500

    Correction Ajout de l'avion.

--- airport_v0_0_1/avion_nouveau.php
+++ airport_v0_0_1/avion_nouveau.php
@@ -19,7 +19,6 @@
             <label for="nom">Nom de l'avion</label> : <input type="text" name="nom" id="nom" /><br />
             <label for="titre">Détails de l'avion</label> : <textarea rows="4" cols="50" type="text" name="autresDetails" id="autresDetails"></textarea><br />
             <label for="nom">Nombre de sièges</label> : <input type="number" name="nbreSieges" id="nbreSieges" value="<?php echo htmlspecialchars($donnees['nbreSieges']); ?>" /><br />
-            <input type="hidden" name="idAvion" value="1" /><br />
             <input type="submit" value="Ajouter" />
         </p>
     </form>
