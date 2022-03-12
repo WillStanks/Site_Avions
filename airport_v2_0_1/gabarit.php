f0c33c397759b8c8e35ab869e31d195cabe0023b
WillStanks <william4800@hotmail.com>
2022-03-12 10:31:00 -0500

    Ajout : Début du modele MVC.

---
+++ airport_v2_0_1/gabarit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title><?= $titre ?></title>
+</head>
+<style>
+    form {
+        text-align: center;
+    }
+</style>
+
+<body>
+    <h2>Avions Air Transat V2.0.4</h2>
+
+    <div id="contenu">
+        <?= $contenu ?>
+    </div>
+
+    <footer id="pied">
+        Site réalisé avec PHP, HTML5 et CSS.
+    </footer>
+</body>
+
+</html>
