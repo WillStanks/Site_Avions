288d1546f01116290ed0eaa8caeaa6c130d1881d
WillStanks <william4800@hotmail.com>
2022-03-31 10:40:14 -0400

    Ajout : Debut nouvelles langues.

--- airport_v2_0_1/Vue/gabarit.php
+++ airport_v2_0_1/Vue/gabarit.php
@@ -14,6 +14,11 @@
 
 <body>
     <div id="global">
+        <header>
+            <a href="#" class="lang-switch" data-locale="fr">Francais</a>
+            <a href="#" class="lang-switch" data-locale="en">English</a>
+            <a href="#" class="lang-switch" data-locale="ro">Romanian</a>
+        </header>
         <h2>Avions Air Transat V3.0.3</h2>
 
         <a href="Vue/a_propos.html">À propos</a></br>
@@ -26,6 +31,18 @@
             Site réalisé avec PHP, HTML5 et CSS.
         </footer>
     </div>
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>
+    <script src="Contenu/js/autocompleteType.js"></script>
+    <script src="Contenu/jquery.i18n/src/CLDRPluralRuleParser.js"></script>
+    <script src="Contenu/jquery.i18n/src/jquery.i18n.js"></script>
+    <script src="Contenu/jquery.i18n/src/jquery.i18n.messagestore.js"></script>
+    <script src="Contenu/jquery.i18n/src/jquery.i18n.fallbacks.js"></script>
+    <script src="Contenu/jquery.i18n/src/jquery.i18n.language.js"></script>
+    <script src="Contenu/jquery.i18n/src/jquery.i18n.parser.js"></script>
+    <script src="Contenu/jquery.i18n/src/jquery.i18n.emitter.js"></script>
+    <script src="Contenu/jquery.i18n/src/jquery.i18n.emitter.bidi.js"></script>
+    <script src="Contenu/i18n/main-jquery_i18n.js"></script>
 </body>
 
 </html>
